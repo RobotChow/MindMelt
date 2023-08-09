e6c79350a0a4a28b116db8fe175c6a722359cdba
Hoch <hoch@Hochs-MacBook-Pro.local>
2023-08-08 19:50:01 -0700

    version 1.5

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/GoesToEleven/puppy"
+)
+
+func main() {
+
+	fmt.Printf("Hello World!\n")
+	fmt.Println(puppy.Bark())
+	fmt.Println(puppy.Barks())
+	// fmt.Println(puppy.From12())
+
+	puppy.From12()
+
+	fmt.Printf("This is version v1.5")
+}
+
+//version v1.5
